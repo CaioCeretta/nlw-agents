11ab8e15c1fec3dd18c6f6a6c289e01b38bac85c
CaioCeretta <caioceretta@gmail.com>
2025-07-11 15:56:50 -0300

    chore: update seed

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -5,18 +5,18 @@ import { schema } from "./schema/index.ts";
 await reset(db, schema);
 
 await seed(db, schema).refine((f) => {
-  return {
-    rooms: {
-      count: 20,
-      columns: {
-        name: f.companyName(),
-        description: f.loremIpsum(),
-      },
-      with: {
-        questions: 10,
-      },
-    },
-  };
+	return {
+		rooms: {
+			count: 5,
+			columns: {
+				name: f.companyName(),
+				description: f.loremIpsum(),
+			},
+			with: {
+				questions: 5,
+			},
+		},
+	};
 });
 
 await sql.end;
